fe08ee779d70181a6ddd3808b5ff1815ef4bdacf
KHI(home) <alfkcjstk853@naver.com>
2023-10-27 11:26:38 +0900

    Player 2의 이동, 점프 키 구현

--- control.py
+++ control.py
@@ -1,7 +1,9 @@
-from sdl2 import SDL_KEYDOWN, SDLK_d, SDLK_a, SDL_KEYUP, SDLK_h
+from sdl2 import SDL_KEYDOWN, SDLK_d, SDLK_a, SDL_KEYUP, SDLK_h, SDLK_RIGHT, SDLK_LEFT, SDLK_SLASH
 
 
 class Player1:
+    x = 200
+    start_face = "r"
     @staticmethod
     def move_r_down(e):
         return e[0] == "INPUT" and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_d
@@ -21,3 +23,26 @@ class Player1:
     @staticmethod
     def jump_down(e):
         return e[0] == "INPUT" and e[1].type == SDL_KEYUP and e[1].key == SDLK_h
+
+class Player2:
+    x = 600
+    start_face = "l"
+    @staticmethod
+    def move_r_down(e):
+        return e[0] == "INPUT" and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_RIGHT
+
+    @staticmethod
+    def move_l_down(e):
+        return e[0] == "INPUT" and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_LEFT
+
+    @staticmethod
+    def move_r_up(e):
+        return e[0] == "INPUT" and e[1].type == SDL_KEYUP and e[1].key == SDLK_RIGHT
+
+    @staticmethod
+    def move_l_up(e):
+        return e[0] == "INPUT" and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
+
+    @staticmethod
+    def jump_down(e):
+        return e[0] == "INPUT" and e[1].type == SDL_KEYUP and e[1].key == SDLK_SLASH
