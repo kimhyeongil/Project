80638e7654ce9d7853964b7949c594c4aad90045
KHI(home) <alfkcjstk853@naver.com>
2023-11-02 18:25:19 +0900

    게임 프레임 웍 구현 그에 따라 게임 매니저를 플레이 모드로 이름 변경

--- play_mode.py
+++ play_mode.py
@@ -2,6 +2,7 @@ from pico2d import open_canvas, delay, close_canvas, get_events, clear_canvas, u
 from sdl2 import SDL_QUIT, SDL_KEYDOWN, SDLK_ESCAPE
 
 import control
+import game_framework
 import mario
 import game_world
 import megamen
@@ -9,23 +10,19 @@ import megamen_projectile
 
 
 def handle_events():
-    global running
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
-            running = False
+            game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            running = False
+            game_framework.quit()
         else:
             player1.state_machine.handle_events(event)
             player2.state_machine.handle_events(event)
 
 
-def create_world():
-    global running
+def init():
     global player1, player2
-    open_canvas()
-    running = True
     megamen_projectile.projectile = load_image('megamen.png')
     player1 = megamen.MegaMen(control.Player1)
     player2 = mario.Mario(control.Player2)
@@ -34,8 +31,10 @@ def create_world():
     game_world.add_obj(player2, 1)
     # game_world.add_obj(megamen.MegaMen(), 1)
 
+def finish():
+    pass
 
-def render():
+def draw():
     clear_canvas()
     game_world.render()
     update_canvas()
@@ -44,11 +43,3 @@ def render():
 def update():
     game_world.update()
 
-
-create_world()
-while running:
-    handle_events()
-    render()
-    update()
-    delay(game_world.time_slice)
-close_canvas()
