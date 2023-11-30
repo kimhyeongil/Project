6468c6e6f880e92654a65aada994192e2685817e
KHI(home) <alfkcjstk853@naver.com>
2023-12-01 01:21:31 +0900

    바닥 추가

--- ground.py
+++ ground.py
@@ -1,9 +1,19 @@
+from pico2d import load_image, get_canvas_width, get_canvas_height
+
+
 class Ground:
     def __init__(self, y):
         self.y = y
+        self.img = load_image("ground.png")
 
     def get_bb(self):
         return -10000, self.y, 10000, self.y
+
+    def draw(self):
+        self.img.clip_draw(0, 0, self.img.w, self.img.h, get_canvas_width() / 2, self.y / 2, get_canvas_width(), self.y)
+
+    def update(self):
+        pass
 
     def handle_collision(self, group, other):
         pass
