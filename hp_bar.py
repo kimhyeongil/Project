4e6c51a3ef672a22f687fb22b47b7934013c5fbc
KHI(home) <alfkcjstk853@naver.com>
2023-12-01 22:57:34 +0900

    체력이 닳기 전 체력바의 크기를 보여주도록 구현

--- hp_bar.py
+++ hp_bar.py
@@ -3,11 +3,13 @@ from pico2d import load_image
 
 class HPBar:
     remain_hp_img = None
+    hp_back_img = None
     size = 250
 
     def __init__(self, O, maxHP, dir):
         if HPBar.remain_hp_img == None:
             HPBar.remain_hp_img = load_image("remain_hp.png")
+            HPBar.hp_back_img = load_image("hp_back.png")
         self.O = O
         self.HP = maxHP
         self.maxHP = maxHP
@@ -16,9 +18,12 @@ class HPBar:
         self.h = HPBar.remain_hp_img.h
 
     def draw(self):
-        self.remain_hp_img.clip_draw(0, 0, self.w, self.h,
-                                     self.O[0] - (self.HP / self.maxHP * HPBar.size * self.dir) // 2, self.O[1],
-                                     (self.HP / self.maxHP * HPBar.size) // 2 * 2, 50)
+        HPBar.hp_back_img.clip_draw(0, 0, self.w, self.h,
+                                    self.O[0] - HPBar.size * self.dir // 2, self.O[1],
+                                    HPBar.size // 2 * 2, 50)
+        HPBar.remain_hp_img.clip_draw(0, 0, self.w, self.h,
+                                      self.O[0] - (self.HP / self.maxHP * HPBar.size * self.dir) // 2, self.O[1],
+                                      (self.HP / self.maxHP * HPBar.size) // 2 * 2, 50)
 
     def update(self):
         pass
